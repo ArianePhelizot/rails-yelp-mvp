825197fc2e703b7cd48c1f10535ce1efa5a2300c
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-05 15:19:11 +0200

    models ok minus rake issue on rating integer

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
 
-  validates :category, inclusion: { in: ["Chinese","Italian","Japanese","French", "Belgian"], allow_nil: true }
+  validates :category, inclusion: { in: ["chinese","italian","japanese","french", "belgian"], allow_nil: false }
   validates :name, presence: true
   validates :address, presence: true
 end
