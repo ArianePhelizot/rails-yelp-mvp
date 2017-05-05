489a227b9db903f1b61e350391a2862c34b22906
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-05 14:16:40 +0200

    seeds created

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  validates :category, inclusion: { in: ["Chinese","Italian","Japanese","French", "Belgian"], allow_nil: true }
+  validates :name, presence: true
+  validates :address, presence: true
 end
