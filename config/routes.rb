ef32efd90767d0ac5744fba09d49fa0cb77e9590
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-08 16:50:06 +0200

    navbar and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'restaurants#index'
 resources :restaurants, only: [:index, :show, :new, :create]
 
 end
+
+
+# config/routes.rb
+# Rails.application.routes.draw do
+#   resources :restaurants do
+#     member do                             # member => restaurant id in URL
+#       get 'chef', to: "restaurants#chef"  # RestaurantsController#chef
+#     end
+#   end
+# end
