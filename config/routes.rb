d3c47ba486457fd370657314de08fbf37734f0b0
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-08 21:52:09 +0200

    achieved

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'restaurants#index'
-resources :restaurants, only: [:index, :show, :new, :create]
+  root 'restaurants#index'
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [ :new, :create ]
+  end
 
 end
-
-
-# config/routes.rb
-# Rails.application.routes.draw do
-#   resources :restaurants do
-#     member do                             # member => restaurant id in URL
-#       get 'chef', to: "restaurants#chef"  # RestaurantsController#chef
-#     end
-#   end
-# end
